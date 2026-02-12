2e17b06df2143ca42c0a4a0d6246822d6d69827b
krista <2479609559@qq.com>
2026-02-12 19:13:53 +0800

    这是我第一次提交，项目初始化

---
+++ demo/src/main/java/com/example/demo/leetcode/hot100/TwoSum.java
@@ -0,0 +1,39 @@
+package com.example.demo.leetcode.hot100;
+
+public class TwoSum {
+    /**
+     * 力扣题目 1：两数之和
+     * 
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，
+     * 并返回它们的数组下标。
+     * 
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     * 
+     * 示例:
+     * 输入: nums = [2, 7, 11, 15], target = 9
+     * 输出: [0, 1]
+     * 解释: 因为 nums[0] + nums[1] == 9 ，返回 [0, 1]。
+     */
+
+    public int[] twoSum(int[] nums, int target) {
+        java.util.Map<Integer, Integer> map = new java.util.HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+            if (map.containsKey(need)) {
+                return new int[] { map.get(need), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        TwoSum solver = new TwoSum();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] ans = solver.twoSum(nums, target);
+        System.out.println("结果下标: [" + ans[0] + ", " + ans[1] + "]");
+    }
+}
+
